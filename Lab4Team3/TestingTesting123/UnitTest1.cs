624330007a87e07c38bae471951bd54aecaba0d8
leepachia <pachia0lee@gmail.com>
2024-10-08 08:06:50 -0500

    Added testing logic for retrieving all airports

--- Lab4Team3/TestingTesting123/UnitTest1.cs
+++ Lab4Team3/TestingTesting123/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Lab2Maui.AirportModel;
+using System.Collections.ObjectModel;
 using Xunit;
 
 namespace TestProject1
@@ -69,6 +70,46 @@ namespace TestProject1
 
             // Assert
             Assert.True(removedAirport); // Checks if deletion was successful
+        }
+
+        public void TestRetrieveAllEntries()
+        {
+            // Arrange
+            IBusinessLogic businessLogic = new BusinessLogic();
+            ObservableCollection<Airport> expectedAirports = new ObservableCollection<Airport>();
+
+            // Airport 1
+            String airport1id = "KATW";
+            String airport1city = "Appleton";
+            DateTime airport1date = new DateTime(2023, 3, 3, 14, 0, 0);
+            int airport1rating  = 3;
+            Airport airport1 = new Airport(airport1id, airport1city, airport1date, airport1rating);
+
+            // Airport 2
+            String airport2id = "KFDL";
+            String airport2city = "Fond du Lac";
+            DateTime airport2date = DateTime.Now;
+            int airport2rating = 4;
+            Airport airport2 = new Airport(airport2id, airport2city, airport2date, airport2rating);
+
+            // Act
+            expectedAirports.Add(airport1);
+            expectedAirports.Add(airport2);
+            businessLogic.AddAirport(airport1id, airport1city, airport1date, airport1rating);
+            businessLogic.AddAirport(airport2id, airport2city, airport2date, airport2rating);
+            ObservableCollection<Airport> actualAirports = businessLogic.GetAirports();
+
+
+            // Assert
+            Assert.Equal(expectedAirports.Count, actualAirports.Count); // Check if counts are equal
+            // Check that each element are equal
+            for (int i = 0; i < expectedAirports.Count; i++)
+            {
+                Assert.Equal(expectedAirports[i].Id, actualAirports[i].Id);
+                Assert.Equal(expectedAirports[i].City, actualAirports[i].City);
+                Assert.Equal(expectedAirports[i].DateVisited, actualAirports[i].DateVisited);
+                Assert.Equal(expectedAirports[i].Rating, actualAirports[i].Rating);
+            }
         }
     }
 }
